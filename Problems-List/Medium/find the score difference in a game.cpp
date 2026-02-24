ba730dffb43677c9ec33c761e919f03f58040232
Sanjat Kumar <143834087+Sanjat123@users.noreply.github.com>
2026-02-24 19:20:27 +0530

    Implement score difference calculation in a game

---
+++ Problems-List/Medium/find the score difference in a game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int scoreDifference(vector<int>& nums) {
+        long long p1 = 0, p2 = 0;
+        bool firstActive = true;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+       
+            if (nums[i] % 2 == 1)
+                firstActive = !firstActive;
+
+        
+            if ((i + 1) % 6 == 0)
+                firstActive = !firstActive;
+
+          
+            if (firstActive) p1 += nums[i];
+            else p2 += nums[i];
+        }
+
+        return (int)(p1 - p2);
+    }
+};
