87ae26034b6b7ab2d71016b1aa3bbcba34f1f451
Sanjat Kumar <143834087+Sanjat123@users.noreply.github.com>
2026-02-25 17:04:28 +0530

    Create DigitorialPermutation.cpp

---
+++ Problems-List/Medium/DigitorialPermutation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isDigitorialPermutation(int n) {
+        int fact[10] = {1,1,2,6,24,120,720,5040,40320,362880};
+
+        string s = to_string(n);
+
+        int sum = 0;
+        vector<int> cnt1(10,0);
+
+        for(char c : s){
+            int d = c-'0';
+            sum += fact[d];
+            cnt1[d]++;
+        }
+
+        string t = to_string(sum);
+
+        if(s.size() != t.size()) return false;
+
+        vector<int> cnt2(10,0);
+        for(char c : t)
+            cnt2[c-'0']++;
+
+        return cnt1 == cnt2;
+    }
+};
