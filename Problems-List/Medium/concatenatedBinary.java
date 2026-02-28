a4eee9077bcf97d95880f08c57f7a0b93ee4209b
Sanjat Kumar <143834087+Sanjat123@users.noreply.github.com>
2026-02-28 14:01:18 +0530

    Create concatenatedBinary.java

---
+++ Problems-List/Medium/concatenatedBinary.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+
+        long ans = 0;
+        int MOD = 1_000_000_007;
+        int bits = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            // if power of 2 → increase bits
+            if ((i & (i - 1)) == 0)
+                bits++;
+
+            ans = ((ans << bits) + i) % MOD;
+        }
+
+        return (int) ans;
+    }
+}
