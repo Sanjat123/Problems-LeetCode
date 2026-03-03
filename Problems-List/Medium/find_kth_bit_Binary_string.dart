a382cee680f09dcd4fd1257dea90846295b7e3e5
Sanjat Kumar <143834087+Sanjat123@users.noreply.github.com>
2026-03-03 22:13:04 +0530

    Refactor findKthBit method for clarity

---
+++ Problems-List/Medium/find_kth_bit_Binary_string.dart
@@ -0,0 +1,18 @@
+class Solution {
+  String findKthBit(int n, int k) {
+    if (n == 1) return "0";
+
+    int len = (1 << n) - 1;    
+    int mid = (len + 1) ~/ 2;
+
+    if (k == mid) return "1";
+
+    if (k < mid) {
+      return findKthBit(n - 1, k);
+    } else {
+      int newK = len - k + 1; 
+      String bit = findKthBit(n - 1, newK);
+      return bit == "0" ? "1" : "0"; 
+    }
+  }
+}
